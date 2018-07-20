ad98bee9fc614153851ea0994c179cbaa20525c6
Indro Lie <Indrolie@gmail.com>
2018-07-20 15:32:41 +0700

    modify level-3 js file

--- manipulation-number/level-3/index.js
+++ manipulation-number/level-3/index.js
@@ -1,1 +1,11 @@
 console.log("JAVASCRIPT");
+
+let x = [-1, 0, 1, 2, 3]
+
+console.log("Array length", x.length)
+
+console.log(a = x[0] + x[4])
+
+console.log(x[0], "adalah bilangan negatif")
+
+console.log(x[0] + x[3] == x[2])
