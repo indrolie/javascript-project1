289accf020cc9ed4a2c30f3cb051df8b28dfdaa8
Indro Lie <Indrolie@gmail.com>
2018-07-22 13:28:31 +0700

    add level 7

--- manipulation-string/level-7/index.js
+++ manipulation-string/level-7/index.js
@@ -1,1 +1,13 @@
 console.log("JAVASCRIPT");
+
+const a = "hELLo wORLd";
+
+function titlecase(str) {
+    b = str.toString();
+
+    return b.replace(/\w\S*/g, function (txt) {
+        return txt.charAt(0).toUpperCase() + txt.substr(1).toLowerCase();
+    });
+}
+
+console.log(titlecase(a));
