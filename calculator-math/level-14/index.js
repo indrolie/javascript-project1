eb2d5007c22c64de37ff0f5e7746e98a08db2905
Indro Lie <Indrolie@gmail.com>
2018-07-22 18:54:50 +0700

    add level 14-15

--- calculator-math/level-14/index.js
+++ calculator-math/level-14/index.js
@@ -1,1 +1,12 @@
 console.log("JAVASCRIPT");
+
+const a = 10;
+const b = 5;
+
+var add = a + b;
+var substract = a - b;
+var multiply = a * b;
+var divide = a / b;
+var modulo = a % b;
+
+console.log(add, substract, multiply, divide, modulo);
--- calculator-math/level-15/index.js
+++ calculator-math/level-15/index.js
@@ -1,1 +1,40 @@
 console.log("JAVASCRIPT");
+
+const a = 10;
+const b = 5;
+
+function add(a, b) {
+    var add = a + b;
+
+    console.log(add);
+}
+
+function substract(a, b) {
+    var substract = a - b;
+
+    console.log(substract);
+}
+
+function multiply(a, b) {
+    var multiply = a * b;
+
+    console.log(multiply);
+}
+
+function divide(a, b) {
+    var divide = a / b;
+
+    console.log(divide);
+}
+
+function modulo(a, b) {
+    var modulo = a % b;
+
+    console.log(modulo);
+}
+
+add(a, b);
+substract(a, b);
+multiply(a, b);
+divide(a, b);
+modulo(a, b);
