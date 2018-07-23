753747bd55cd46608efd11cbe1b7418470d445f8
Indro Lie <Indrolie@gmail.com>
2018-07-23 11:50:02 +0700

    add level 16

--- calculator-math/level-16/index.js
+++ calculator-math/level-16/index.js
@@ -1,1 +1,19 @@
 console.log("JAVASCRIPT");
+
+function add(x, y) {
+    const add = x + y;
+    return add
+}
+
+function substract(a, b) {
+    const substract = a - b;
+    return substract
+}
+
+function multiply(a, b) {
+    const multiply = a * b;
+    return multiply
+}
+
+const result = add(substract(10, 5), multiply(10, 5));
+console.log(result);
