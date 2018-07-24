ead180c7efad00f58b180ff1460809ba484600c9
Indro Lie <Indrolie@gmail.com>
2018-07-24 20:49:08 +0700

    add level 21 and 22

--- calculator-time/level-21/index.js
+++ calculator-time/level-21/index.js
@@ -1,1 +1,16 @@
 console.log("JAVASCRIPT");
+
+const a = new Date();
+console.log(a);
+
+const b = new Date(2018, 8, 12, 13, 21, 49, 0);
+console.log(b);
+
+const c = new Date(18, 8, 12, 13, 21, 49, 0);
+console.log(c);
+
+const d = new Date("October 13, 2014 11:13:00");
+console.log(d);
+
+const e = new Date("13 October 2014 11:13:00 PM");
+console.log(e);
--- calculator-time/level-22/index.js
+++ calculator-time/level-22/index.js
@@ -1,1 +1,11 @@
 console.log("JAVASCRIPT");
+
+const button = document.getElementById("buttonGetTime");
+
+const getTime = () => {
+  const time = new Date();
+
+  document.getElementById("time").innerHTML = time;
+};
+
+button.addEventListener("click", getTime);
