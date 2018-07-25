6344c3d324edf98b0a648fdc65cf19a8bfd4da07
Indro Lie <Indrolie@gmail.com>
2018-07-25 22:39:17 +0700

    add level 25

--- address-book/level-25/index.js
+++ address-book/level-25/index.js
@@ -1,1 +1,25 @@
+"use strict";
+
 console.log("JAVASCRIPT");
+
+const buttonAdd = document.getElementById("button-add");
+
+const checkemail = () => {
+  try {
+    event.preventDefault();
+    let email = document.getElementById("email").value;
+
+    const pattern = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+    let check = pattern.test(email);
+
+    if (check === false) {
+      alert("Please enter a correct email address!");
+    }
+  } catch (error) {
+    document.write(
+      "Sorry for the inconvenience, but we're having a problem. Please try again later."
+    );
+  }
+};
+
+buttonAdd.addEventListener("click", checkemail);
