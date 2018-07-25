0583c733dcc77fb2e35f7db2fc5f3955a19baa6a
Indro Lie <Indrolie@gmail.com>
2018-07-25 15:18:43 +0700

    add level 23

--- calculator-time/level-23/index.js
+++ calculator-time/level-23/index.js
@@ -1,1 +1,14 @@
 console.log("JAVASCRIPT");
+
+const button = document.getElementById("buttonGetTime");
+
+const bornYear = () => {
+  const age = 22;
+  const time = new Date();
+  const year = time.getFullYear();
+  const result = year - age;
+
+  document.getElementById("time").innerHTML = result;
+};
+
+button.addEventListener("click", bornYear);
